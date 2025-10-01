8977237d8935027c00bcbedcacd8fdb07bcca41f
Francesco Carcangiu <francesco.carcangiu3@gmail.com>
2025-10-01 11:46:04 +0200

    upload modifiche

--- DLL_Injector/DLL_Injector/main.cpp
+++ DLL_Injector/DLL_Injector/main.cpp
@@ -1,30 +1,30 @@
 /*
-A DLL injector that injects the DLL specified in dll_path into the wesnoth process. This injector requires Urban Terror to be running.
+Un iniettore di DLL che inietta la DLL specificata in dll_path nel processo wesnoth. Questo iniettore richiede che Urban Terror sia in esecuzione.
 
-A different process can be specified by changing the executable name in the strcmp call.
+Un processo diverso può essere specificato cambiando il nome dell'eseguibile nella chiamata a strcmp.
 
-To load static and dynamic libraries, Windows executables can use the LoadLibraryA API function. This function takes a single argument,
-which is the full path of the library to load:
+Per caricare librerie statiche e dinamiche, gli eseguibili Windows possono usare la funzione API LoadLibraryA. Questa funzione prende un singolo argomento,
+che è il percorso completo della libreria da caricare:
 
 HMODULE LoadLibraryA(
 	LPCSTR lpLibFileName
 );
 
-If we call LoadLibraryA in the code of our injector, the DLL will be loaded into the memory of our injector. Instead, we want our injector to force the game
-to call LoadLibraryA. To do this, we will use the CreateRemoteThread API to create a new thread in the game. That thread will then execute LoadLibraryA
-inside the running game process.
+Se chiamiamo LoadLibraryA nel codice del nostro iniettore, la DLL verrà caricata nella memoria del nostro iniettore. Invece, vogliamo che il nostro iniettore forzi il gioco
+a chiamare LoadLibraryA. Per fare ciò, useremo l'API CreateRemoteThread per creare un nuovo thread nel gioco. Questo thread eseguirà quindi LoadLibraryA
+all'interno del processo in esecuzione del gioco.
 
-However, since the thread runs inside the game's memory, LoadLibraryA will not be able to find the path to our DLL specified in our injector.
-To work around this, we must write the path of our DLL into the game's memory. To ensure we do not corrupt other memory, we will also
-allocate additional memory inside the game using VirtualAllocEx.
+Tuttavia, poiché il thread viene eseguito all'interno della memoria del gioco, LoadLibraryA non sarà in grado di trovare il percorso della nostra DLL specificato nel nostro iniettore.
+Per ovviare a questo, dobbiamo scrivere il percorso della nostra DLL nella memoria del gioco. Per assicurarci di non corrompere altra memoria, dovremo anche
+allocare memoria aggiuntiva all'interno del gioco usando VirtualAllocEx.
 
-The full explanation of how this code works is available at https://gamehacking.academy/lesson/25
+La spiegazione completa di come funziona questo codice è disponibile su https://gamehacking.academy/lesson/25
 */
 #include <windows.h>
 #include <tlhelp32.h>
 
 
-// The full path of the DLL to inject.
+// Il percorso completo della DLL da iniettare.
 const char* dll_path = "C:\\Users\\francesco\\source\\repos\\InternalMemoryHack\\Debug\\InternalMemoryHack.dll";
 
 int main(int argc, char** argv) {
@@ -35,33 +35,33 @@ int main(int argc, char** argv) {
 
 	pe32.dwSize = sizeof(PROCESSENTRY32);
 
-	// The snapshot code is a reduced version of the Microsoft example at
+	// Il codice per lo snapshot è una versione ridotta dell'esempio fornito da Microsoft su 
 	// https://docs.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
 	snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	Process32First(snapshot, &pe32);
 
 	do {
-		// We only want to operate on the wesnoth process
+		// Vogliamo operare solamente sul processo di wesnoth
 		if (wcscmp(pe32.szExeFile, L"wesnoth.exe") == 0) {
-			// First, we need to obtain a handle to the process to use for subsequent calls
+			// Per prima cosa, dobbiamo ottenere un handle del processo da usare per le chiamate successive
 			HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, true, pe32.th32ProcessID);
 
-			// To avoid corrupting existing memory, allocate extra memory to hold the path to our DLL
+			// Per non corrompere memoria esistente, allocare memoria aggiuntiva per contenere il percorso della nostra DLL
 			void* lpBaseAddress = VirtualAllocEx(process, NULL, strlen(dll_path) + 1, MEM_COMMIT, PAGE_READWRITE);
 
-			// Write the path of our DLL into the memory we just allocated inside the game
+			// Scrivere il percorso della nostra DLL nella memoria che abbiamo appena allocato dentro il gioco
 			WriteProcessMemory(process, lpBaseAddress, dll_path, strlen(dll_path) + 1, NULL);
 
-			// Create a remote thread inside the game that will execute LoadLibraryA
-			// We will pass to this LoadLibraryA call the full path of our DLL that we wrote into the process
+			// Creare un thread remoto all'interno del gioco che eseguirà LoadLibraryA
+			// A questa chiamata LoadLibraryA passeremo il percorso completo della nostra DLL che abbiamo scritto nel processo
 			HMODULE kernel32base = GetModuleHandle(L"kernel32.dll");
 			HANDLE thread = CreateRemoteThread(process, NULL, 0, (LPTHREAD_START_ROUTINE)GetProcAddress(kernel32base, "LoadLibraryA"), lpBaseAddress, 0, NULL);
 
-			// To ensure our DLL was injected, we can use the two calls below to block program execution
+			// Per assicurarci che la nostra DLL sia stata iniettata, possiamo usare le due chiamate seguenti per bloccare l'esecuzione del programma
 			WaitForSingleObject(thread, INFINITE);
 			GetExitCodeThread(thread, &exitCode);
 
-			// Finally free the memory and clean up the process handles
+			// Infine liberare la memoria e pulire gli handle di processo
 			VirtualFreeEx(process, lpBaseAddress, 0, MEM_RELEASE);
 			CloseHandle(thread);
 			CloseHandle(process);
