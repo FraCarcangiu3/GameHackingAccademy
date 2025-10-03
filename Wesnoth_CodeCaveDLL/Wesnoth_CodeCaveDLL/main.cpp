fb3f67dd1113571da1812c304bcb7ea6354c100c
Francesco Carcangiu <francesco.carcangiu3@gmail.com>
2025-10-03 12:47:37 +0200

    fix

--- Wesnoth_CodeCaveDLL/Wesnoth_CodeCaveDLL/main.cpp
+++ Wesnoth_CodeCaveDLL/Wesnoth_CodeCaveDLL/main.cpp
@@ -1,113 +1,63 @@
 /*
-	Una DLL che reindirizza la funzione "Descrizione Terreno" di Wesnoth 1.14.9 a una funzione personalizzata che imposta l'oro del giocatore a 888.
-	Questa funzione personalizzata poi ricrea la funzione "Descrizione Terreno" e restituisce l'esecuzione al programma.
+	A DLL that redirects the "Terrain Description" function Wesnoth 1.14.9 to a custom function that sets the player's gold to 888.
+	This custom function then recreates the "Terrain Description" function and returns execution to the program.
 
-	Questo viene fatto tramite l'uso di una codecave. Quando iniettata, la DLL modifica la funzione che mostra la
-	descrizione del terreno e cambia il codice per saltare alla funzione codecave definita nella DLL. La funzione codecave
-	salva poi i registri, imposta l'oro a 888, e poi ripristina le istruzioni originali modificate
-	prima di ritornare al codice chiamante originale.
+	This is done through the use of a codecave. When injected, the DLL modifies the function that displays the
+	terrain description and changes the code to jump to the codecave function defined in the DLL. The codecave
+	function then saves the registers, sets the gold to 888, and then restores the original modified instructions
+	before returning to the original calling code.
 
-	Questo deve essere iniettato nel processo di Wesnoth per funzionare. Un modo per farlo � usare un injector di DLL.
-	Un altro modo � abilitare AppInit_DLLs nel registro.
+	This must be injected into the Wesnoth process to work. One way to do this is to use a DLL injector.
+	Another way is to enable AppInit_DLLs in the registry.
 
-	Gli offset e la spiegazione della codecave sono trattati in https://gamehacking.academy/lesson/11 e https://gamehacking.academy/lesson/17
+	The offsets and explanation of the codecave are covered in https://gamehacking.academy/lesson/11 and https://gamehacking.academy/lesson/17
 */
-
 #include <Windows.h>
-#pragma comment(lib, "user32.lib")
-
 
 DWORD* player_base;
 DWORD* game_base;
 DWORD* gold;
-DWORD ret_address = 0x00CCAF90; // indirizzo di ritorno esatto (32-bit)
-// test
+DWORD ret_address = 0xCCAF90;
 
-// Handle dell'evento creato dall'inniettore (aperto con OpenEvent)
-static HANDLE hInjectorEvent = NULL;
-static HMODULE g_hModule = NULL;
-
-// Dichiarazione forward
-void SignalInjector();
-
-// Codecave: esegue patch lato gioco e poi richiama SignalInjector
+// Our codecave that program execution will jump to. The declspec naked attribute tells the compiler to not add any function
+// headers around the assembled code
 __declspec(naked) void codecave() {
-    __asm { pushad }    // salva registri
+	// Asm blocks allow you to write pure assembly
+	// In this case, we use it to save all the registers
+	__asm {
+		pushad
+	}
 
-    // codice C++ normale (puoi spostarlo in una funzione se preferisci)
-    // Nota: non usare molte API complesse direttamente qui; qui chiami una funzione semplice.
-    // Impostare l'oro
-    //player_base = (DWORD*)0x037EEC88;
-    player_base = (DWORD*)0x017EED18;
-    game_base = (DWORD*)(*player_base + 0xA90);
-    gold = (DWORD*)(*game_base + 4);
-    *gold = 888;
+	// Set the player's gold in the same method discussed in https://gamehacking.academy/lesson/16
+	player_base = (DWORD*)0x037EEC88;
+	game_base = (DWORD*)(*player_base + 0xA90);
+	gold = (DWORD*)(*game_base + 4);
+	*gold = 888;
 
-    __asm { popad }     // ripristina registri
-
-    // Chiamiamo una piccola funzione C che segnala l'event all'inniettore (se presente)
-    SignalInjector();
-
-    // Ripristiniamo le istruzioni originali (esempio) e saltiamo all'indirizzo di ritorno:
-    __asm {
-        // riproduci eventuali istruzioni sovrascritte (esempio illustrativo)
-        mov eax, dword ptr ds : [ecx]
-        lea esi, dword ptr ds : [esi]
-        // salta direttamente all'indirizzo di ritorno
-        jmp ret_address
-    }
+	// Restore the registers and then recreate the original instructions that we overwrote
+	// After those, jump back to the instruction after the one we overwrote
+	_asm {
+		popad
+		mov eax, dword ptr ds : [ecx]
+		lea esi, dword ptr ds : [esi]
+		jmp ret_address
+	}
 }
 
-// Funzione che fa SetEvent sull'event aperto dall'InitThread
-void SignalInjector() {
-    if (hInjectorEvent) {
-        SetEvent(hInjectorEvent);
-    }
-}
-
-// Thread di inizializzazione: esegue VirtualProtect + patch e apre l'evento
-DWORD WINAPI InitThread(LPVOID lpv) {
-    // Apri l'event endpoint creato dall'inniettore (se esiste)
-    hInjectorEvent = OpenEventA(EVENT_MODIFY_STATE, FALSE, "WesnothHookEvent");
-    // NON � un errore se OpenEvent fallisce: l'inniettore potrebbe non averlo creato.
-
-    // esegui la patch come prima
-    DWORD old_protect;
-    unsigned char* hook_location = (unsigned char*)0x00CCAF8A;
-
-    if (VirtualProtect((void*)hook_location, 6, PAGE_EXECUTE_READWRITE, &old_protect)) {
-        *hook_location = 0xE9; // JMP rel32
-        *(DWORD*)(hook_location + 1) = (DWORD)&codecave - ((DWORD)hook_location + 5);
-        *(hook_location + 5) = 0x90; // NOP per coprire 1 byte rimanente
-
-        // assicurati che la CPU veda le nuove istruzioni
-        FlushInstructionCache(GetCurrentProcess(), hook_location, 6);
-
-        // ripristina protezioni
-        VirtualProtect((void*)hook_location, 6, old_protect, &old_protect);
-
-        // segnale visivo per confermare che la patch è stata applicata
-        MessageBoxA(NULL, "Hook applicato: codecave installata.", "Wesnoth_CodeCaveDLL", MB_OK | MB_ICONINFORMATION);
-    } else {
-        MessageBoxA(NULL, "VirtualProtect fallita su hook_location.", "Wesnoth_CodeCaveDLL", MB_OK | MB_ICONERROR);
-    }
-
-    // Thread pu� terminare: non deve rimanere bloccato in DllMain.
-    return 0;
-}
-
+// When our DLL is attached, unprotect the memory at the code we wish to write at
+// Then set the first opcode to E9, or jump
+// Caculate the location using the formula: new_location - original_location+5
+// Finally, since the original instructions totalled 6 bytes, NOP out the last remaining byte
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-    g_hModule = (HMODULE)hinstDLL;
+	DWORD old_protect;
+	unsigned char* hook_location = (unsigned char*)0x00CCAF8A;
 
-    if (fdwReason == DLL_PROCESS_ATTACH) {
-        // Evitiamo chiamate potenzialmente bloccanti in DllMain:
-        // creiamo subito un thread che far� il resto.
-        HANDLE h = CreateThread(NULL, 0, InitThread, NULL, 0, NULL);
-        if (h) CloseHandle(h);
-    }
-    else if (fdwReason == DLL_PROCESS_DETACH) {
-        // pulizia
-        if (hInjectorEvent) { CloseHandle(hInjectorEvent); hInjectorEvent = NULL; }
-    }
-    return TRUE;
+	if (fdwReason == DLL_PROCESS_ATTACH) {
+		VirtualProtect((void*)hook_location, 6, PAGE_EXECUTE_READWRITE, &old_protect);
+		*hook_location = 0xE9;
+		*(DWORD*)(hook_location + 1) = (DWORD)&codecave - ((DWORD)hook_location + 5);
+		*(hook_location + 5) = 0x90;
+	}
+
+	return true;
 }
