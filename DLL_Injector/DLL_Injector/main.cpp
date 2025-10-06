7d35ddd48e7d9ea1485b96f84f5760677217509d
Francesco Carcangiu <francesco.carcangiu3@gmail.com>
2025-10-06 10:41:31 +0200

    add StatHACK

--- DLL_Injector/DLL_Injector/main.cpp
+++ DLL_Injector/DLL_Injector/main.cpp
@@ -27,8 +27,11 @@ La spiegazione completa di come funziona questo codice � disponibile su https:
 
 
 // Il percorso completo della DLL da iniettare.
+//const char* dll_path = "C:\\Users\\francesco\\source\\repos\\..\\Debug\\.dll";
 //const char* dll_path = "C:\\Users\\francesco\\source\\repos\\InternalMemoryHack\\Debug\\InternalMemoryHack.dll";
-const char* dll_path = "C:\\Users\\francesco\\source\\repos\\Wesnoth_CodeCaveDLL\\Debug\\Wesnoth_CodeCaveDLL.dll";
+//const char* dll_path = "C:\\Users\\francesco\\source\\repos\\Wesnoth_CodeCaveDLL\\Debug\\Wesnoth_CodeCaveDLL.dll";
+const char* dll_path = "C:\\Users\\francesco\\source\\repos\\Wesnoth_Stathack\\Debug\\Wesnoth_Stathack.dll";
+
 
 // Helper di errore: centralizza la stampa di GetLastError con messaggio leggibile
 static void stampaErrore(const char* dove) {
