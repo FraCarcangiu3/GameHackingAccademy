d881446738f7a19a4ebcae0edda48ed592ac1521
Francesco Carcangiu <f.carcangiu5@studenti.unica.it>
2025-10-07 18:37:11 +0200

    update

--- UrbanTerror_MemoryWallhack/UrbanTerror_MemoryWallhack/main.cpp
+++ UrbanTerror_MemoryWallhack/UrbanTerror_MemoryWallhack/main.cpp
@@ -1,12 +1,12 @@
 /*
-Un wallhack per Urban Terror 4.3.4 che rivela le entità attraverso i muri disabilitando il depth-testing.
+Un wallhack per Urban Terror 4.3.4 che rivela le entitï¿½ attraverso i muri disabilitando il depth-testing.
 
-Questo viene fatto modificando il flag di rendering di ogni entità, che è responsabile di determinare come l'entità deve essere renderizzata.
-Impostando questo valore sul valore di gioco corrispondente al depth testing disabilitato (0xD), le entità verranno disegnate indipendentemente dal fatto che dovrebbero essere visibili o meno.
-Il codice agganciato è un'istruzione mov che avviene dopo che ebx è stato caricato con una struttura entità valida.
+Questo viene fatto modificando il flag di rendering di ogni entitï¿½, che ï¿½ responsabile di determinare come l'entitï¿½ deve essere renderizzata.
+Impostando questo valore sul valore di gioco corrispondente al depth testing disabilitato (0xD), le entitï¿½ verranno disegnate indipendentemente dal fatto che dovrebbero essere visibili o meno.
+Il codice agganciato ï¿½ un'istruzione mov che avviene dopo che ebx ï¿½ stato caricato con una struttura entitï¿½ valida.
 
-Questo deve essere iniettato nel processo di Urban Terror per funzionare. Un modo per farlo è usare un injector di DLL.
-Un altro modo è abilitare AppInit_DLLs nel registro.
+Questo deve essere iniettato nel processo di Urban Terror per funzionare. Un modo per farlo ï¿½ usare un injector di DLL.
+Un altro modo ï¿½ abilitare AppInit_DLLs nel registro.
 
 Gli offset e il metodo per scoprirli sono discussi nell'articolo su: https://gamehacking.academy/lesson/22
 */
@@ -14,12 +14,12 @@ Gli offset e il metodo per scoprirli sono discussi nell'articolo su: https://gam
 
 DWORD ret_address = 0x0052D303;
 
-// La nostra codecave a cui l'esecuzione del programma salterà. L'attributo declspec naked dice al compilatore di non aggiungere
+// La nostra codecave a cui l'esecuzione del programma salterï¿½. L'attributo declspec naked dice al compilatore di non aggiungere
 // header di funzione attorno al codice assemblato
 __declspec(naked) void codecave() {
 	// I blocchi Asm permettono di scrivere puro assembly
 	// In questo caso li usiamo per salvare tutti i registri
-	// Poi impostiamo il valore di render dell'entità a [ebx+4] su depth testing disabilitato (0xD)
+	// Poi impostiamo il valore di render dell'entitï¿½ a [ebx+4] su depth testing disabilitato (0xD)
 	// Poi ripristiniamo i registri, ricreiamo l'istruzione originale, e saltiamo indietro al codice del programma
 	__asm {
 		pushad
@@ -34,16 +34,16 @@ __declspec(naked) void codecave() {
 // Quando la nostra DLL viene allegata, rimuovi la protezione dalla memoria nel punto in cui vogliamo scrivere
 // Poi imposta il primo opcode a E9, ossia un jump
 // Calcola la posizione usando la formula: new_location - original_location + 5
-// Infine, poiché le istruzioni originali erano in totale 6 byte, riempi con NOP l'ultimo byte rimanente
+// Infine, poichï¿½ le istruzioni originali erano in totale 6 byte, riempi con NOP l'ultimo byte rimanente
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
 	DWORD old_protect;
 	unsigned char* hook_location = (unsigned char*)0x0052D2FD;
 
 	if (fdwReason == DLL_PROCESS_ATTACH) {
 		VirtualProtect((void*)hook_location, 5, PAGE_EXECUTE_READWRITE, &old_protect);
-		*hook_location = 0xE9;
-		*(DWORD*)(hook_location + 1) = (DWORD)&codecave - ((DWORD)hook_location + 5);
-		*(hook_location + 5) = 0x90;
+		*hook_location = 0xE9; // jump
+		*(DWORD*)(hook_location + 1) = (DWORD)&codecave - ((DWORD)hook_location + 5); // jump to codecave
+		*(hook_location + 5) = 0x90; // nop
 	}
 
 	return true;
